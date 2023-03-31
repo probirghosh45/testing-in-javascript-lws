5145b6c2c75e56d04b7da9dd66c87d1e8fb6c8e8
probir ghosh <probirghosh@outlook.com>
2023-03-31 19:54:28 +0600

    input value validation test done

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,5 +1,9 @@
-const {generateResult} = require("../js/lib")
+const {generateResult,validateInput} = require("../js/lib")
 
 test("testing generate result", () => {
-  expect(generateResult("1","our first test")).toBe("User ID: 1 created an article titled our first test")
+  expect(generateResult("1","our first test")).toBe("User ID: 1 created an article titled our first test") 
 });
+
+test("testing Input Value", () => {
+    expect(validateInput(1,true,true)).toBeTruthy()
+ }) 
